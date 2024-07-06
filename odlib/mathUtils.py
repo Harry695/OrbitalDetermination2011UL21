e3ee15a1c1e0accf1f9e98c810d262ee2a970176
Harry Ouyang <oyjunhan@hotmail.com>
2024-07-05 22:00:54 -0500

    Implement active rotation matrices for vectors

--- odlib/mathUtils.py
+++ odlib/mathUtils.py
@@ -1,4 +1,11 @@
 from math import*
+import numpy as np
+from enum import Enum
+
+class Axis(Enum):
+    X = "X"
+    Y = "Y"
+    Z = "Z"
 
 def quadrantCorrection(sin, cos): #TODO: test edge cases more
     thetaS = degrees(asin(sin))
@@ -14,3 +21,23 @@ def quadrantCorrection(sin, cos): #TODO: test edge cases more
     else:
         return 0
     
+def vectorRotation(vec, axis, angle): # active rotation
+    vec = np.matrix([[vec[0]], [vec[1]], [vec[2]]]) #convert 1x3 to 3x1 for matrix multiplication
+    angle = radians(angle)
+    match axis:
+        case Axis.X:
+            rotMatrix = np.array([[1, 0, 0],
+                                   [0, cos(angle), -sin(angle)],
+                                   [0, sin(angle), cos(angle)]])
+        case Axis.Y:
+            rotMatrix = np.array([[cos(angle), 0, sin(angle)],
+                                  [0, 1, 0],
+                                  [-sin(angle), 0, cos(angle)]])
+        case Axis.Z:
+            rotMatrix = np.array([[cos(angle), -sin(angle), 0],
+                                  [sin(angle), cos(angle), 0],
+                                  [0, 0, 1]])
+    res = rotMatrix @ vec
+    return np.array([res[0, 0], res[1, 0], res[2, 0]])
+
+ 
