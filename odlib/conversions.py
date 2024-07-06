149e199d131ae446d18145a56327144486557e87
Harry Ouyang <oyjunhan@hotmail.com>
2024-07-05 20:53:53 -0500

    Implement reverse angle conversions

--- odlib/conversions.py
+++ odlib/conversions.py
@@ -1,3 +1,5 @@
+from math import*
+
 def HMStoDeg(h, m, s):
     m = s / 60 + m
     h = m / 60 + h
@@ -5,5 +7,25 @@ def HMStoDeg(h, m, s):
 
 def DMStoDeg(d, m, s):
     m = s / 60 + m
-    m = m*-1 if d < 0 else m
+    m = m*-1 if d < 0 else m # convert m to sign of d for addition
     return m / 60 + d
+
+def printHMS(ra):
+    hoursDecimal = ra / 360 * 24
+    h = floor(hoursDecimal)
+    hoursDecimal -= h
+    m = floor(hoursDecimal * 60)
+    hoursDecimal -= m/60
+    s = round(hoursDecimal * 3600, 2)
+    print(f'''RA: {h}h {m}' {s}"''')
+
+def printDMS(dec):
+    d = -1 if dec < 0 else 1
+    dec = abs(dec)
+    d = d * floor(dec)
+    dec -= abs(d)
+    dec = abs(dec)
+    m = floor(dec * 60)
+    dec -= m/60
+    s = round(dec * 3600, 2)
+    print(f'''DEC: {d}deg {m}' {s}"''')
