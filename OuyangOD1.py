33f9ffe161cb5c474c987cd7c988285ab9c7c3c8
Harry Ouyang <oyjunhan@hotmail.com>
2024-07-05 23:10:11 -0500

    Finish calculation of angular momentum in OD#1

---
+++ OuyangOD1.py
@@ -0,0 +1,29 @@
+from odlib.constants import*
+from odlib.conversions import*
+from odlib.mathUtils import*
+from odlib.orbitalCharacteristics import*
+import numpy as np
+import math
+
+file = open("OuyangInput.txt").readlines()
+qf15 = []
+for i in range(len(file)-2): # len(file)-2 to get rid of annoying stuff at the end. See the file.
+    item = file[i].strip()
+    qf15.append(float(item))
+
+# test asteroid 2002 QF15
+posVec = np.array([qf15[0], qf15[1], qf15[2]])
+velVec = np.array([qf15[3], qf15[4], qf15[5]]) * Constants.DAY_IN_GAUSSIAN_DAY
+print("Specific Angular momentum:", getAngularMomentum(posVec, velVec))
+
+
+#DEBUG
+# print("HMStoDeg test", HMStoDeg(12, 3, 5.3)) #expected 180.7720833
+# print("DMStoDeg test", DMStoDeg(-13, 45, 23.45)) #expected -13.75651389
+# deg = 0
+# print("Quadrant correction for", deg, quadrantCorrection(sin(math.radians(deg)), cos(math.radians(deg))))
+# print("RAdecimalToHMS test 180.7720833:")
+# printHMS(180.7720833)
+# print("DecDecimalToDMS test 180.7720833:")
+# printDMS(-13.75651389)
+# print("VectorRotation test:\n", vectorRotation(np.array([-0.23, 0.887, 0.34]), Axis.Y, 45))
