34eed63fe6fe95cca3e8c5380d1d7b018a8aae2a
Harry Ouyang <oyjunhan@hotmail.com>
2024-07-09 21:03:14 -0500

    Implement functional plate constant code

--- LSPR.py
+++ LSPR.py
@@ -4,27 +4,70 @@ from odlib.conversions import*
 # parse data
 file = "LSPRtestinput1.txt"
 data = [e.strip() for e in open(file).readlines()]
-dataMatrix = []
+dataMat = []
 for obj in data:
-    dataMatrix.append(obj.split())
-dataMatrix = np.array(dataMatrix).transpose() # easier to do stuff to rows to column
+    dataMat.append(obj.split())
+dataMat = np.array(dataMat).transpose() # easier to do stuff to rows to column
 
 print(data) # debug
-print(dataMatrix) # debug
+print(dataMat) # debug
 
 # numberify all values
-dataMatrix[0] = [float(e) for e in dataMatrix[0]]# x
-dataMatrix[1] = [float(e) for e in dataMatrix[1]]# y
-for i in range(len(dataMatrix[2])): # ra
-    ra = [float(e) for e in dataMatrix[2, i].split(":")]
-    dataMatrix[2, i] = HMStoDeg(ra[0], ra[1], ra[2])
-for i in range(len(dataMatrix[3])): # dec
-    dec = [float(e) for e in dataMatrix[3, i].split(":")]
-    dataMatrix[3, i] = DMStoDeg(dec[0], dec[1], dec[2]) 
-
-print(dataMatrix) #debug
+for i in range(len(dataMat[2])): # ra
+    ra = [float(e) for e in dataMat[2, i].split(":")]
+    dataMat[2, i] = HMStoDeg(ra[0], ra[1], ra[2])
+for i in range(len(dataMat[3])): # dec
+    dec = [float(e) for e in dataMat[3, i].split(":")]
+    dataMat[3, i] = DMStoDeg(dec[0], dec[1], dec[2]) 
+dataMat = dataMat.astype(float)
+print(dataMat) #debug
 
 # form matrix variables
-n = len(dataMatrix[0]) 
-print(n)
+n = len(dataMat[0]) 
+print("n", n) #debug
+sumX, sumY, sumRA, sumDEC = np.sum(dataMat, axis=1)
+print("sumX", sumX) #checked
+print("sumY", sumY) #checked
+print("sumRA", sumRA)
+print("sumDEC", sumDEC)
+sumX2 = np.sum(dataMat[0]**2)
+print("sumX2", sumX2) #checked
+sumY2 = np.sum(dataMat[1]**2)
+print("sumY2", sumY2) #checked
+sumXY = np.sum(dataMat[0] * dataMat[1])
+print("sumXY", sumXY)
+sumRAX = np.sum(dataMat[0] * dataMat[2])
+print("sumRAX", sumRAX)
+sumRAY = np.sum(dataMat[1] * dataMat[2])
+print("SumRAY", sumRAY)
+sumDECX = np.sum(dataMat[0] * dataMat[3])
+print("SumDECX", sumDECX)
+sumDECY = np.sum(dataMat[1] * dataMat[3])
+print("SumDECY", sumDECY)
 
+# building matrices
+matXY = np.linalg.inv(np.matrix([[n, sumX, sumY],
+                   [sumX, sumX2, sumXY],
+                   [sumY, sumXY, sumY2]]))
+print("matXY", matXY)
+matRA = np.matrix([[sumRA],
+                   [sumRAX],
+                   [sumRAY]])
+print("matRA", matRA)
+matDEC = np.matrix([[sumDEC],
+                    [sumDECX],
+                    [sumDECY]])
+print("matDEC", matDEC)
+
+# multiply matrices
+mat1 = matXY @ matRA
+mat2 = matXY @ matDEC
+# debug
+for e in mat1:
+    print(e)
+for e in mat2:
+    print(e)
+
+#assigning variables
+b1, a11, a12 = mat1
+b2, a21, a22 = mat2
